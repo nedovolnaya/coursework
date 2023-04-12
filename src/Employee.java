da1015d2156dd0fcc926e9cf05eadd89003129cd
Unteryana <ishtar_v@bk.ru>
2023-04-12 11:41:59 +0300

    написан класс Employee

---
+++ src/Employee.java
@@ -0,0 +1,38 @@
+public class Employee {
+    String name;
+    int department;
+    int salary;
+    int id;
+
+    public Employee(String name, int department, int salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+
+    public String getName() {
+        return this.name = name;
+    }
+
+    public int getDepartment() {
+        return this.department = department;
+    }
+
+    public int getSalary() {
+        return this.salary = salary;
+    }
+
+    public int getId() {
+        return this.id = id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
+
